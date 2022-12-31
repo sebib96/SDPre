db68f381693d23aabe026b1ee27bd3597543320d
sebib96 <sebastiantellob96@hotmail.com>
2022-12-31 01:02:11 -0500

    complete objective2lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,10 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    int age = 1996;
+    String month = "June";
+    boolean weekend = true;
+    System.out.println(age);
+    System.out.println(month);
+    System.out.println(weekend);
+  }
+}
