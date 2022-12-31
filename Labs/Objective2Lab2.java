ec2564e8af3e0f0ea30540aa34b15e47cc5bdf75
sebib96 <sebastiantellob96@hotmail.com>
2022-12-31 00:55:19 -0500

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,8 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Sebastian Tello ";
+    int age = 26;
+    String state = " Florida";
+    System.out.println(name + age + state);
+  }
+}
