09cae15894e2f5e13d97094c3a07798b38330c67
sebib96 <sebastiantellob96@hotmail.com>
2023-01-09 13:40:46 -0500

    complete objective6lab1

---
+++ Labs/Objective6Lab1.java
@@ -0,0 +1,18 @@
+public class Objective6Lab1 {
+  public static void main(String[] args) {
+    int month = 6;
+
+    switch (month) {
+      case 1:
+        System.out.println("January");
+        break;
+      case 6:
+        System.out.println("June");
+        break;
+
+      default:
+        System.out.println("That is not a valid month");
+        break;
+    }
+  }
+}
