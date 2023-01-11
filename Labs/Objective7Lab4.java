565b157e9b36d522ded1b98d96d83b9f84b139cf
sebib96 <sebastiantellob96@hotmail.com>
2023-01-11 11:53:29 -0500

    complete objective7lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,12 @@
+public class Objective7Lab4 {
+	public static void main(String[] args) {
+	int counter = 0; 
+	int sum = 0;
+
+	while(counter <= 19) {
+		counter ++;
+		sum += counter;
+	}
+	System.out.println(sum);
+}
+}
