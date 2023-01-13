f299a5212ad6a661c641a08c0fcad2a7398d21cf
sebib96 <sebastiantellob96@hotmail.com>
2023-01-13 12:19:23 -0500

    complete objective8lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,12 @@
+public class Objective8Lab1 {
+	public static void main (String [] args) {
+		int i = 0;
+		for (i = 1; i <= 10; i++){
+		System.out.println(i);
+	}
+
+		for(i = 10; i >=1; i--){
+			System.out.println(i);
+		}
+	}
+}
